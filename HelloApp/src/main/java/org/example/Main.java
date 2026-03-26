794cec898996144a6406f41a890e1d759413b696
shreyyy00 <sb5019@srmist.edu.in>
2026-03-26 08:10:57 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -4,8 +4,11 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HelloApp {
     public static void main(String[] args ) {
-        String name=args[0];
-        System.out.println("Hello, " + name + "!");
+        String name="World";
 
+        if (args.length > 0){
+            name = args[0];
+        }
+        System.out.println("Hello, " + name + "!");
     }
 }
